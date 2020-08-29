641ce0395db9580db1465c5dc4c7d3a8fa39732b
Aad Versteeg <aad.versteeg@transavia.com>
2020-08-29 14:03:08 +0200

    feat: first version of package

---
+++ src/Ave.Extensions.Text/Luhn32.cs
@@ -0,0 +1,41 @@
+﻿namespace Ave.Extensions.Text
+{
+    /// <summary>
+    /// Adaptation of luhns algorithm of using checksums for base 32.
+    /// 
+    /// Ported from https://github.com/syncthing/syncthing
+    /// 
+    /// </summary>
+    public static class Luhn32
+    {
+        private const string digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+
+        public static char GetCheckSum(string base32)
+        {
+            var factor = 1;
+            var sum = 0;
+            var n = 32;
+
+            for (var i = 0; i < base32.Length; i++)
+            {
+                var value = digits.IndexOf(base32[i]);
+
+                var addend = factor * value;
+
+                factor = factor == 1 ? 2 : 1;
+                addend = (addend / n) + (addend % n);
+                sum += addend;
+            }
+
+            var remainder = sum % n;
+            var valueCode = (n - remainder) % n;
+            return digits[valueCode];
+        }
+
+        public static bool IsValid(string base32)
+        {
+            var indexOfLast = base32.Length - 1;
+            return GetCheckSum(base32.Substring(0, indexOfLast)) == base32[indexOfLast];
+        }
+    }
+}
---
+++ src/UnitTests.Extensions.Text/Luhn32Tests.cs
@@ -0,0 +1,47 @@
+﻿using Ave.Extensions.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests.Extensions.Text
+{
+    public class Luhn32Tests
+    {
+        [Theory(DisplayName = "L-001: GetCheckSum should return correct check sum.")]
+        [InlineData("BW7L2X7R4RRAN", 'V')]
+        [InlineData("SHALM2JMHCEYY", 'Q')]
+        [InlineData("435PYOXWDDHW2", 'D')]
+        [InlineData("6UVNHCZNET4GA", 'Z')]
+        [InlineData("PVMAG27UJZUO4", 'P')]
+        [InlineData("RR5FHL6XWRC4M", '3')]
+        [InlineData("47A5UF2C65GLH", 'S')]
+        [InlineData("J6MCDUFZHSZOA", 'F')]
+
+        public void L001(string base32, char expectedCheck)
+        {
+            // act
+            var check = Luhn32.GetCheckSum(base32);
+
+            // assert
+            check.Should().Be(expectedCheck);
+        }
+
+        [Theory(DisplayName = "L-002: IsValid should return true when valid, otherwise false.")]
+        [InlineData("BW7L2X7R4RRANV", true)]
+        [InlineData("SHALM2JMHCEYYQ", true)]
+        [InlineData("435PYOXWDDHW2D", true)]
+        [InlineData("6UVNHCZNET4GAZ", true)]
+        [InlineData("PVMAG27UJZUO4J", false)]
+        [InlineData("RR5FHL6XWRC4MD3", false)]
+        [InlineData("47A5UF2C65GLHA", false)]
+        [InlineData("J6MCDUFZHSZOAC", false)]
+
+        public void L002(string base32, bool expectedValid)
+        {
+            // act
+            var isValid = Luhn32.IsValid(base32);
+
+            // assert
+            isValid.Should().Be(expectedValid);
+        }
+    }
+}
